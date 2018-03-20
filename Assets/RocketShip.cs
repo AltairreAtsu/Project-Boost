2e54f5d757554f617e1bbeeee2d5ae685498cfae
Marc Taurasi <altairre.atsu@gmail.com>
2018-03-20 16:58:40 -0600

    Add foundation for rocket Momvement

---
+++ Assets/RocketShip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShip : MonoBehaviour {
+    private Rigidbody rigidBody;
+
+    [SerializeField]
+    private float thrust = 1;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+        }
+
+    }
+}
