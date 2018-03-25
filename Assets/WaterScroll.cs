8a93b1f89806182f4e4d2f0fc7c26de92727186f
Marc Taurasi <altairre.atsu@gmail.com>
2018-03-25 11:04:44 -0600

    Added new water prefab and water testing level.

---
+++ Assets/WaterScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScroll : MonoBehaviour {
+
+	private Renderer renderer = null;
+	[SerializeField] private Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+	private Vector2 uvOffset = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer>();
+	}
+
+	void Update () {
+		uvOffset += (uvAnimationRate * Time.deltaTime);
+		renderer.material.SetTextureOffset("_MainTex", uvOffset);
+	}
+}
